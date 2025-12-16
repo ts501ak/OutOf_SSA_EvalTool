fd8580eda90d70873ff9fdb84a9e3025e6d3cf74
Jannis Schröder <jannis.schroeder@fkie.fraunhofer.de>
2025-12-16 04:17:36 +0100

    Updated structuring for more transparency of the control flow

--- dependencyGraphfromC.py
+++ dependencyGraphfromC.py
@@ -9,10 +9,11 @@ class DependencyGraphfromCFunction:
         """As this class will compute a few thousand dependency graphs we precompile all our regexes in advance, so we don't have to do it every single run"""
 
         self.__regs = []
-        self.__regs.append(re.compile(r'"(\\.|[^"\\])*"')) #No Strings
-        self.__regs.append(re.compile(r"'(\\.|[^'\\])*'")) #No Strings
         self.__regs.append(re.compile(r"//.*")) #No Comments
         self.__regs.append(re.compile(r"/\*.*?\*/",re.DOTALL)) #No Multiline Comments
+        self.__regs.append(re.compile(r'"(\\.|[^"\\])*"')) #No Strings --> To be removed
+        self.__regs.append(re.compile(r"'(\\.|[^'\\])*'")) #No Strings --> To be removed
+        self.__stringIdent = re.compile(r"\$[a-fA-F0-9]{64}\$")
         self.__doubleNL = re.compile(r"\n\n+")
         self.__doubleWS = re.compile(r"  +")
         self.__col = re.compile(r" *, *")
@@ -35,20 +36,24 @@ class DependencyGraphfromCFunction:
         self.__funcEnd = re.compile(r"[A-Za-z_]\w*$")
         self.__funcNorm = re.compile(r"[A-Za-z_]\w*\(")
         self.__oneequals = re.compile("[^=]=[^=]")
-        self.__noConstant = re.compile(r"^ *0x.*")
-        self.__pat = r"(\+|\-(?!>)|\*|/|%|==|!=|(?<!-)>|<|>=|<=|\&\&|\|\||\&|\||\^|<<|>>|\)|\(|,|\[|\])"
+        self.__Constant = re.compile(r"^ *(?P<num>(0x([0-9]|[ABCDEF]|[abcdef])+|(\+|-)?[0-9]+\.[0-9]+((E|e)?(\+|-)?[0-9]+)?|0b[10]+|(\+|-)?[0-9]+))")
+        self.__pat = r"((?<!(E|e))\+(?!(>|\+|-))|(?<!(E|e))\-(?!(>|\+|-))|\*|/|%|==|!=|>=|<=|(?<!-)>|<|\&\&|\|\||\&|\||\^|<<|>>|\)|\(|,|\[|\])"
         self.__specialAccess = re.compile(r" *(?P<first>[A-Za-z_]\w*) *((\.|->) *[A-Za-z_]\w*)*")
         self.__numberbegin = re.compile(r"^ *[0-9]")
         self.__ternaryAssig = re.compile(r"\?(?P<ifTrue>[^:\n]*):(?P<ifFalse>[^;\n]*)")
         self.__dotVar = re.compile(r"(^ *\. *[A-Za-z_]\w*|^ *-> *[A-Za-z_]\w*)")
+        self.__counter = 0
+        self.__newLine = re.compile(" *\n *")
+        
 
 
-    def getDependencyGraph(self,cFunc : str) -> nx.Graph:
+    def getDependencyGraph(self,cFunc : str) -> nx.DiGraph:
         """takes the code of a single function as ascii and builds the dependency graph for it"""
 
 
         self.__func = cFunc
         self.__prepareFunc()
+        self.__counter = 0
         return self.__constructDepGraph()
 
     def __prepareFunc(self):
@@ -70,69 +75,119 @@ class DependencyGraphfromCFunction:
         self.__func = self.__sc.sub(r";",self.__func)
         self.__func = self.__wsBeginning.sub(r"\n",self.__func)
         self.__func = self.__wsEquals.sub(r"=",self.__func)
+        self.__func = self.__newLine.sub(r" ",self.__func)
 
 
-    def __constructDepGraph(self) -> nx.Graph:
+    def __constructDepGraph(self) -> nx.DiGraph:
         """computes the dependency graph of a given preprocessed function
             Tenary assignments are ignored"""
         
-        self.depGraph = nx.Graph()
+        self.depGraph = nx.DiGraph()
         
-        for line in self.__func.split("\n"):
-            for instr in [x for x in line.split(";") if x != '']:
-                if any(aop in instr for aop in self.__assignOperators) and (not "return" in instr) and (self.__oneequals.search(instr)): #No == as Assignmet Operator
-                    if assi := self.__variableAssign.search(instr):
-                        lhs = assi.group("first")
-                        if (ind := assi.group(0).find("[")) != -1:
-                            self.__getVariablesOfString2(instr[ind+1:len(assi.group(0))-1],lhs)
-                        start = instr.find(assi.group(0))
-                        instr = instr[start + len(assi.group(0)):]
-                        if ta := self.__ternaryAssig.search(instr):
-                            self.__getVariablesOfString(ta.group("ifTrue"),lhs,False)
-                            self.__getVariablesOfString(ta.group("ifFalse"),lhs,False)
-                        else:
-                            self.__getVariablesOfString(instr,lhs,False)
-                
-                elif (ind := instr.find("return")) != -1:
-                    if ta := self.__ternaryAssig.search(instr[ind + 6:]):
-                            self.__getVariablesOfString(ta.group("ifTrue"),"return",True)
-                            self.__getVariablesOfString(ta.group("ifFalse"),"return",True)
+        #for line in self.__func.split("\n"):
+        for instr in [x for x in self.__func.split(";") if x != '']:
+            if any(aop in instr for aop in self.__assignOperators) and (not "return" in instr) and (self.__oneequals.search(instr)): #No == as Assignmet Operator
+                if assi := self.__variableAssign.search(instr):
+                    lhs = assi.group("first")
+                    #if (ind := assi.group(0).find("[")) != -1: #No more detection of Array access, bc this may get lost after OoSSA or is added afterwards
+                    #    self.__getVariablesOfString2(instr[ind+1:len(assi.group(0))-1],lhs)
+                    start = instr.find(assi.group(0))
+                    instr = instr[start + len(assi.group(0)):]
+                    if ta := self.__ternaryAssig.search(instr):
+                        nodeName = self.__getVarName()
+                        self.depGraph.add_edge(nodeName,lhs)
+                        self.__getVariablesOfString(ta.group("ifTrue"),nodeName)
+                        nodeName = self.__getVarName()
+                        self.depGraph.add_edge(nodeName,lhs)
+                        self.__getVariablesOfString(ta.group("ifFalse"),nodeName)
                     else:
-                        self.__getVariablesOfString(instr[ind + 6:],"return",True)
+                        nodeName = self.__getVarName()
+                        self.depGraph.add_edge(nodeName,lhs)
+                        self.__getVariablesOfString(instr,nodeName)
+            
+            elif (ind := instr.find("return")) != -1:
+                if ta := self.__ternaryAssig.search(instr[ind + 6:]):
+                    nodeName = self.__getVarName()
+                    self.depGraph.add_edge(nodeName,"return")
+                    self.__getVariablesOfString(ta.group("ifTrue"),nodeName)
+                    nodeName = self.__getVarName()
+                    self.depGraph.add_edge(nodeName,"return")
+                    self.__getVariablesOfString(ta.group("ifFalse"),nodeName)
+                else:
+                    nodeName = self.__getVarName()
+                    self.depGraph.add_edge(nodeName,"return")
+                    self.__getVariablesOfString(instr[ind + 6:],nodeName)
 
 
         self.depGraph.remove_edges_from(list(nx.selfloop_edges(self.depGraph)))
         return self.depGraph
     
-    def __getVariablesOfString(self,strIn : str,lhs,ret:bool = False):
+    def __getVarName(self):
+        nodeName = f"$help{self.__counter}$"
+        self.__counter += 1
+        return nodeName
+
+    def __getVariablesOfString(self,strIn : str,lhs:str):
         instr = re.split(self.__pat,strIn)
-        instr = [i for i in instr if i != '']
+        instr = [i for i in instr if (i != '') and (i != None)]
         instr.append(";")
-        for i in range(len(instr)-1):
-            tok = instr[i]
+        count = -1
+        print(instr)
+        while (count) < (len(instr) - 1):
+            tok = instr[count]
             if self.__types.search(tok):
-                continue
+                pass
             elif (tok in self.__operators) or (tok == ","):
-                continue
-            elif self.__noConstant.search(tok) != None:
-                continue
+                pass
+            elif (const := self.__Constant.search(tok)) != None:
+                self.depGraph.add_edge(const.group("num"),lhs,type="const")
+                
             elif (tok == "(") or (tok == ")"):
-                continue
-            elif (f := self.__funcEnd.search(tok)) and (instr[i+1] == "("):
-                if not ret:
-                    self.depGraph.add_edge(f.group(0),lhs,type="func")
-                else:
-                    self.depGraph.add_edge(f.group(0),lhs,type="return")
-            elif (v := self.__specialAccess.search(tok)) and (not self.__numberbegin.search(tok)) and (not self.__noConstant.search(tok)):
-                if not ret:
-                    self.depGraph.add_edge(v.group("first"),lhs,type="var")
-                else:
-                    self.depGraph.add_edge(v.group("first"),lhs,type="return")
+                pass
+            elif string := self.__stringIdent.search(tok):
+                self.depGraph.add_edge(string.group(0),lhs)
+            elif (f := self.__funcEnd.search(tok)) and (instr[count+1] == "("):
+                funcName = f"{f.group(0)}${self.__counter}"
+                self.__counter += 1
+                self.depGraph.add_edge(funcName,lhs,type="func")
+                brcount = 0
+                for i in range(count+1,len(instr)-1):
+                    if instr[i] == "(":
+                        brcount += 1
+                    elif instr[i] == ")":
+                        brcount -= 1
+                    if brcount == 0:
+                        brcount = i
+                        break
+                rest = "".join(instr[count+1:brcount+1])
+                self.__getVariablesOfString(rest,funcName)
+                count = brcount + 1    #This is why we use while(index < int) instead of for i in range(int)
+            elif (f := self.__funcEnd.search(tok)) and (instr[count+1] == "["): #Array (but with Function Variables :) Sorry)
+                funcName = f"{f.group(0)}${self.__counter}"
+                self.__counter += 1
+                self.depGraph.add_edge(funcName,lhs,type="array")
+                brcount = 0
+                for i in range(count+1,len(instr)-1):
+                    if instr[i] == "[":
+                        brcount += 1
+                    elif instr[i] == "]":
+                        brcount -= 1
+                    if brcount == 0:
+                        brcount = i
+                        break
+                rest = "".join(instr[count+1:brcount+1])
+                self.__getVariablesOfString(rest,funcName)
+                count = brcount + 1    #This is why we use while(index < int) instead of for i in range(int)
+                
+            elif (v := self.__specialAccess.search(tok)) and (not self.__numberbegin.search(tok)) and (not self.__Constant.search(tok)):
+                self.depGraph.add_edge(v.group("first"),lhs,type="var")
             else:
-                continue
+                pass
+            count += 1
         return
     
-    def __getVariablesOfString2(self,strIn : str,lhs,rek = 0): #special Case for [...]
+    """ def __getVariablesOfString2(self,strIn : str,lhs,rek = 0): #This Function got NO update
+        bet = []
         instr = re.split(self.__pat,strIn)
         instr = [i for i in instr if i != '']
         instr.append(";")
@@ -155,9 +210,10 @@ class DependencyGraphfromCFunction:
                 continue
 
         return
+    """
 
 
-    def areSimilar(self,g1 :nx.Graph, g2 : nx.Graph):
+    def areSimilar(self,g1 :nx.DiGraph, g2 : nx.DiGraph):
         pass
 
 def main():
@@ -167,9 +223,10 @@ def main():
     dg = dgrc.getDependencyGraph(cCode)
     for x in dg.edges():
         print(x)
-    ix.network(dg,layout=nx.arf_layout(dg),node_labels=True)
-    ax = pp.gca()
-    ax.set_facecolor("#bfbfbf")
+    pso = nx.spring_layout(dg)
+    nx.draw_networkx_nodes(dg,pso,dg.nodes())
+    nx.draw_networkx_edges(dg,pso,dg.edges(),arrows=True,arrowstyle="->")
+    nx.draw_networkx_labels(dg,pso)
     pp.show()
 
 
